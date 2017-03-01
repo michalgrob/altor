1e5ae8f44096d364ab9aef3729b958ad9926616d
maor davidzon <maorda@mta.ac.il>
2017-03-01 20:18:02 +0200

    changed to html from jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', sender: 'MAor' });
+  res.render('maor', { title: 'Express', sender: 'MAor' });
 });
 
 module.exports = router;
